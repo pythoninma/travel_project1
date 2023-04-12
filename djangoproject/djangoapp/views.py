7143666dc25ffb64f50bab0a54a63a53d8ee7391
Avinash Vinay <avinashvinay@Avinashs-MacBook-Air.local>
2023-04-12 12:23:44 +0530

    added

---
+++ djangoproject/djangoapp/views.py
@@ -0,0 +1,17 @@
+
+from django.shortcuts import render
+from .models import Place, People
+
+
+# Create your views here.
+def demo(request):
+    obj=Place.objects.all()
+    obj1=People.objects.all()
+    return  render(request,"index.html",{'result':obj,'results':obj1})
+
+
+
+
+
+
+
